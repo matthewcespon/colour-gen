48a08cbd0e6685865a4f6a13e4c5d66b4e63b7f4
Matthew Cespon <mattces1@yahoo.com>
2024-12-07 22:58:38 +0000

    remove unused

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
 const ListItem = React.forwardRef<
   React.ElementRef<'a'>,
   React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => {
+>(({ title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
